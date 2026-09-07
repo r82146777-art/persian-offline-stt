23951975f8ebc22795037f689c59ad6fea1c52e2
r82146777-art <r82146777@gmail.com>
2026-09-07 21:56:06 +0330

    Bump to 1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.persianstt.offline"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.5.0"
+        versionCode = 8
+        versionName = "1.6.0"
     }
 
     buildTypes {
