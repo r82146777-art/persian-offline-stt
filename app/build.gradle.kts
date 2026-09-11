aedf4dd5cd7beb9be628963fa9a7a7108bc41e15
r82146777-art <r82146777@gmail.com>
2026-09-11 03:35:35 +0330

    v2.9.0: fix number regex + auto language + space bar + settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.persianstt.offline"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "2.8.0"
+        versionCode = 29
+        versionName = "2.9.0"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
         }
